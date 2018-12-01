3074fe62d9fc055332f2dad16db25fa3259c93b6
Joshua Weggartner <Joshua.Weggartner@gmx.de>
2018-12-01 21:03:21 +0100

    hud

---
+++ GyroskopeTest/Assets/Scripts/Scanbutton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scanbutton : MonoBehaviour {
+
+    public Image image;
+    public Button button;
+    public float RechargeTime;
+    private float time;
+  
+
+	// Use this for initialization
+	void Start () {
+        time = 0;
+        button.enabled = true;
+        image.fillAmount = 1;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!button.enabled)
+        {
+            time = time + Time.deltaTime;
+            image.fillAmount = image.fillAmount + Time.deltaTime/RechargeTime;
+            Debug.Log(time);
+            if (time >= RechargeTime)
+            {
+                time = 0;
+                Debug.Log("enabling");
+                button.enabled = true;
+                image.fillAmount = 1;
+            }
+        }
+
+	}
+
+    public void BeginnScan(){
+        //Scanmethod
+
+        button.enabled = false;
+        image.fillAmount = 0;
+
+    }
+
+
+}
