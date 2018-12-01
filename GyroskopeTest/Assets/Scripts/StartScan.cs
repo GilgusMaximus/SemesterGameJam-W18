8ff6f642ee02603406aed7bb767117107fec51fa
Unknown <philipp@mewww.de>
2018-12-01 21:11:33 +0100

    Script for scannerbutton and updated scannerfield

---
+++ GyroskopeTest/Assets/Scripts/StartScan.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScan : MonoBehaviour {
+
+    public GameObject scanner;
+    public float scanTime = 3.0f;
+    public float scanOfftime = 2.0f;
+    public bool buttonActive = true;
+    private float scanTimeLeft;
+    private float scanOfftimeLeft;
+
+	// Use this for initialization
+	void Start () {
+        buttonActive = true;
+        scanTimeLeft = scanTime;
+        scanOfftimeLeft = scanOfftime;
+        scanner.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(scanner.activeSelf) {
+            scanTimeLeft -= Time.deltaTime;
+            if(scanTimeLeft < 0) {
+                scanner.SetActive(false);
+                scanTimeLeft = scanTime;
+                buttonActive = false;
+            }
+        }
+        else {
+            if (!buttonActive) {
+                scanOfftimeLeft -= Time.deltaTime;
+                if(scanOfftimeLeft < 0) {
+                    scanOfftimeLeft = scanOfftime;
+                    buttonActive = true;
+                }
+            } 
+        }
+	}
+
+    void Startscan() {
+        scanner.SetActive(true);
+    }
+}
