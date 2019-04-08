554f0ed18a468472d6d4ee6d491cf9a5b3ef5fe4
Luca <hohmann.luca@gmx.de>
2019-04-08 16:09:01 +0200

    Options Menu Functionality Added: Options Menu Functionality - Except for the name change, all buttons do work

--- GyroskopeTest/Assets/Scripts/UI/StartMenuUIController.cs
+++ GyroskopeTest/Assets/Scripts/UI/StartMenuUIController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class StartMenuUIController : MonoBehaviour
 {
@@ -10,22 +12,58 @@ public class StartMenuUIController : MonoBehaviour
 	private Animator playButtonAnimator;
 
 	[SerializeField]
-	private Animator[] startupMenu;
+	private Animator[] startupMenu, optionsMenu;
+	[SerializeField]
+	private GameObject startUpMenuParent;
 	
-	//TODO Node: im new state zwischen FadeIn und FadeOut muss Write defaults aus sein
+	[SerializeField]
+	private Sprite[] muteSprites = new Sprite[2];
+
+
+	[SerializeField] 
+	private Image muteButtonImage;
+
+	[SerializeField] private AudioSource backgroundMusic;
+
+	private bool isAudioMuted = false; 
+	
+	private int currentMuteInt = 1;
+	//------------------------------------------------------------------------
+	//                          General
+	//------------------------------------------------------------------------	
+	
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+			triggerFadeIn();
+	}
+
 
 	private void Start()
 	{
-		
+		triggerFadeIn();
 	}
 
-	public void triggerFadeOut()
+	//------------------------------------------------------------------------
+	//                          StartupMenu
+    //------------------------------------------------------------------------
+    
+	public void triggerFadeOut(int menuID)
 	{
 
+		Animator[] animators = null;
+
+		switch (menuID){
+			case 0: animators = optionsMenu;
+				
+				break;
+		}
 		//trigger reset to false after being activated
 		for(int i = 0; i < startupMenu.Length; i++)
 			startupMenu[i].SetTrigger("PlayFadeOut");
 		
+		for(int i = 0; i < animators.Length; i++)
+			animators[i].SetTrigger("PlayFadeIn");
 		
 	}
 	
@@ -39,9 +77,40 @@ public class StartMenuUIController : MonoBehaviour
 		
 	}
 
-	private void Update()
+	public void exitProsougram()
 	{
-		if(Input.GetKeyDown(KeyCode.A))
-			triggerFadeIn();
+		Application.Quit();
+	}
+	
+	//------------------------------------------------------------------------
+	//                          OptionsMenu
+	//------------------------------------------------------------------------
+
+	//changes between the 2 mute images
+	public void muteAudio(){
+		muteButtonImage.sprite = muteSprites[currentMuteInt++];
+		currentMuteInt %= 2;
+		isAudioMuted = !isAudioMuted;
+		backgroundMusic.mute = isAudioMuted;
+	}
+
+	public void sliderValueChanged(Slider slider)
+	{
+		backgroundMusic.volume = slider.value/100f;
+	}
+
+	public void backToStartupmenu(int menuID){
+		//menu 0 = options
+		Animator[] animators = null;
+		switch (menuID){
+			case 0: animators = optionsMenu;
+				break;
+		}
+
+		for (int i = 0; i < animators.Length; i++){
+			animators[i].SetTrigger("PlayFadeOut");
+		}
+		for(int i = 0; i < startupMenu.Length; i++)
+			startupMenu[i].SetTrigger("PlayFadeIn");
 	}
 }
