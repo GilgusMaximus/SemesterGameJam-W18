87a2f7e24fbec3651ff4f38fa7df92f8ec162e5c
Luca <hohmann.luca@tutanota.com>
2019-04-14 15:02:24 +0200

    3D model to level choose menu
    Added:
    3D model to level choose menu - first cave model is now displayed in the level choose menu

---
+++ GyroskopeTest/Assets/Scripts/UI/LevelChooseMenuModelSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChooseMenuModelSpin : MonoBehaviour {
+    
+    
+	private Transform transform;
+
+	[SerializeField]
+	private float spinningSpeed = 5f; 
+	// Use this for initialization
+	void Start (){
+		transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * spinningSpeed, Space.World);
+	}
+}
