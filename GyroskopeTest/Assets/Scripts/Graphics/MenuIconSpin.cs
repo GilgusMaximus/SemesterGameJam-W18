7c40634dbaac1a0ba8a5a7ed303dc1b9d9b7f545
Luca <hohmann.luca@tutanota.com>
2019-09-11 17:53:45 +0200

    Menu Logo Added: Menu Logo - Logo is now displayed in menu and also spins

---
+++ GyroskopeTest/Assets/Scripts/Graphics/MenuIconSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuIconSpin : MonoBehaviour
+{
+
+	[SerializeField] private RectTransform iconTransform;
+	// Update is called once per frame
+	void Update () {
+		iconTransform.Rotate(new Vector3(0, 1, 0), Space.Self);
+	}
+}
