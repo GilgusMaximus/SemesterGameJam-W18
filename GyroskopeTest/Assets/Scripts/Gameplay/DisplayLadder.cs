30eb0f93ab1b60576021ecf6a23040519dc5b3f2
Bussler <maarten.bussler@gmail.com>
2019-10-08 14:15:14 +0200

    Krass Formatting

--- GyroskopeTest/Assets/Scripts/Gameplay/DisplayLadder.cs
+++ GyroskopeTest/Assets/Scripts/Gameplay/DisplayLadder.cs
@@ -31,6 +31,8 @@ public class DisplayLadder : MonoBehaviour {
 
     public void OnHighscoresDownloaded(Highscore[] hList)
     {
+        doFormatting(hList, 8);
+
         for (int i = 0; i < highscoreText.Length; i++)
         {
             highscoreText[i].text = i + 1 + ". ";
@@ -38,6 +40,18 @@ public class DisplayLadder : MonoBehaviour {
             {
                 highscoreText[i].text += hList[i].username + " - " + hList[i].score;
             }
+        }
+    }
+
+    public void doFormatting(Highscore[] hList, int numName)//Format the highscores so they fit in the UI
+    {
+        for (int i = 0; i < highscoreText.Length; i++)
+        {
+            if (hList[i].username.Length >= numName)
+            {
+                hList[i].username = hList[i].username.Substring(0, 5)+"..";
+            }
+
         }
     }
 
